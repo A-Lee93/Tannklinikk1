91df69f377811e0f01acc78a06b5b9cee3394e2f
A-Lee93 <31926595+A-Lee93@users.noreply.github.com>
2017-09-18 15:23:46 +0200

    Update Appointment.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,11 +1,21 @@
+import java.util.Calendar;
+
 public class Appointment
 {
     private Tannlege tannlege;
     private Patient patient;
+    private int hour, minute;
 
-    public Appointment(Tannlege tannlege, Patient patient)
+    public Appointment(Tannlege tannlege, Patient patient, Calendar calendar)
     {
         this.tannlege = tannlege;
         this.patient = patient;
+        this.hour = calendar.get(calendar.HOUR);
+        this.minute = calendar.get(calendar.MINUTE);
     }
+    public String getAppointment()
+    {
+        return "Appointment at: " + hour + ":" + minute;
+    }
+
 }
