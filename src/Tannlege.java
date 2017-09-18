bd0d1846e7f8f80456979c28257f7f740aa4745f
A-Lee93 <31926595+A-Lee93@users.noreply.github.com>
2017-09-18 15:23:15 +0200

    Update Tannlege.java

--- src/Tannlege.java
+++ src/Tannlege.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Tannlege extends Person
@@ -5,10 +6,12 @@ public class Tannlege extends Person
     private int lonn;
     private String spesialitet;
     private static int ansattID = 0;
+    private ArrayList<Appointment> appointments;
 
     public Tannlege(String fornavn, String etternavn, String adresse, int telefon, Calendar birthdate, int lonn, String spesialitet)
     {
         super(fornavn,etternavn,adresse,telefon,birthdate);
+        appointments = new ArrayList<>();
         this.lonn = lonn;
         this.spesialitet = spesialitet;
         ansattID++;
@@ -32,5 +35,26 @@ public class Tannlege extends Person
     public int getLonn()
     {
         return lonn;
+    }
+    public void addAppointment(Appointment appointment)
+    {
+        boolean exist = false;
+        for(Appointment app : appointments)
+        {
+            if(appointment.getAppointment().equalsIgnoreCase(app.getAppointment()))
+            {
+                exist = true;
+                break;
+            }
+        }
+        if(exist == true)
+        {
+            System.out.println("Cannot add appointment at this time! One already exist");
+        }
+        else
+        {
+            appointments.add(appointment);
+            System.out.println("Appointment added");
+        }
     }
 }
