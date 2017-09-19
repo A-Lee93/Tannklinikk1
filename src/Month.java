cde5c0a2285017802c0985ed57312adfa24ffbae
A-Lee93 <31926595+A-Lee93@users.noreply.github.com>
2017-09-19 16:26:55 +0200

    Update Month.java

--- src/Month.java
+++ src/Month.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 public class Month
 {
     private ArrayList<Day> days;
+    private int mnd;
+    private String mndNavn;
+
     public Month(ArrayList<Day> days)
     {
         this.days = days;
+
     }
     public Day getDay(int day)
     {
