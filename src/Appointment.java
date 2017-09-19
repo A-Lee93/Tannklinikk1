8b53963562d463a6a59254caa8bb2df7c53320c5
A-Lee93 <31926595+A-Lee93@users.noreply.github.com>
2017-09-19 16:29:00 +0200

    Update Appointment.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -4,7 +4,7 @@ public class Appointment
 {
     private Tannlege tannlege;
     private Patient patient;
-    private int hour, minute;
+    private int hour, minute, day, month, year;
 
     public Appointment(Tannlege tannlege, Patient patient, Calendar calendar)
     {
@@ -12,10 +12,19 @@ public class Appointment
         this.patient = patient;
         this.hour = calendar.get(calendar.HOUR);
         this.minute = calendar.get(calendar.MINUTE);
+        this.day = calendar.get(calendar.DATE);
+        this.month = calendar.get(calendar.MONTH);
+        this.year = calendar.get(calendar.YEAR);
     }
     public String getAppointment()
     {
         return "Appointment at: " + hour + ":" + minute;
+    }
+    public Calendar getAppointmentDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year,month,day,hour,minute,0);
+        return cal;
     }
 
 }
